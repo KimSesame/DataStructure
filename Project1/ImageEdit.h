320092ea37e1d394093919e086be1874cdc8c8ce
KimSesame <kimbs082787@gmail.com>
2022-10-07 22:28:26 +0900

    Make frame of image editor

---
+++ Project1/ImageEdit.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "iostream"
+#include "fstream"
+
+#define HEIGHT 256
+#define WIDTH 256
+
+void flip(std::string i_route, std::string o_route = "C:/Users/kimbs/repos/Assignments/DataStructure/Project1/Result");
+void light(std::string i_route, std::string o_route = "C:/Users/kimbs/repos/Assignments/DataStructure/Project1/Result", int value);
+void resize(std::string i_route, std::string o_route = "C:/Users/kimbs/repos/Assignments/DataStructure/Project1/Result");
+
+void flip(std::string i_route, std::string o_route = "C:/Users/kimbs/repos/Assignments/DataStructure/Project1/Result")
+{
+    
+}
+
+void light(std::string i_route, std::string o_route = "C:/Users/kimbs/repos/Assignments/DataStructure/Project1/Result", int value)
+{
+}
+
+void resize(std::string i_route, std::string o_route = "C:/Users/kimbs/repos/Assignments/DataStructure/Project1/Result")
+{
+}
